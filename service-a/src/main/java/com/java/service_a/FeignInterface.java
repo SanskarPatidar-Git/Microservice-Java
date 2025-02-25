97dd99f9edd1da347a2e8bf00113cd51aa266d01
Sanskar <sanskarpatidar14@gmail.com>
2025-02-25 17:08:57 +0530

    Microservice run

--- service-a/src/main/java/com/java/service_a/FeignInterface.java
+++ service-a/src/main/java/com/java/service_a/FeignInterface.java
@@ -8,6 +8,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+/*
+ * @FeignClient-
+ * It is used for calling an another service with less boilerplate code.
+ * It needs Service registry to find the service at runtime.
+ * When we are using RestTemplate we need to specify the URL but when app runs in production with multiple instances we don't know IP and port at early stage
+ * so it needs service registry to identify it. In our case we used Eureka Server.
+ * 
+ * @FeignClient("SERVICE-B")-
+ * Here we describe the service name which contains that methods which we need to call.
+ * It is simply interface which contains method signature.
+ */
+
 @FeignClient("SERVICE-B")
 public interface FeignInterface {
 
