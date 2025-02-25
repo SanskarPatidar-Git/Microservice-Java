c06e5e0cb489fad922fe110c890f1490a1b2e211
Sanskar <sanskarpatidar14@gmail.com>
2025-02-25 16:07:02 +0530

    Feign and eureka setup done

--- service-a/src/main/java/com/java/service_a/AService.java
+++ service-a/src/main/java/com/java/service_a/AService.java
@@ -2,17 +2,21 @@ package com.java.service_a;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AService {
 
+	@Autowired
+	FeignInterface feignInterface;
+	
 	public ResponseEntity<String> save(SkillsDto skill) {
-		return null;
+		return feignInterface.saveSkill(skill);
 	}
 	
 	public ResponseEntity<List<SkillsDto>> getAll() {
-		return null;
+		return feignInterface.getAllSkills();
 	}
 }
--- service-a/src/main/java/com/java/service_a/FeignInterface.java
+++ service-a/src/main/java/com/java/service_a/FeignInterface.java
@@ -1,8 +1,20 @@
 package com.java.service_a;
 
+import java.util.List;
+
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
-
+@FeignClient("SERVICE-B")
 public interface FeignInterface {
 
+	@PostMapping("skills/save")
+	public ResponseEntity<String> saveSkill(@RequestBody SkillsDto skill);
+	
+	
+	@GetMapping("skills/getall")
+	public ResponseEntity<List<SkillsDto>> getAllSkills();
 }
